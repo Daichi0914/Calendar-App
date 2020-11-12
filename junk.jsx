9636f2adb80fced4db79eced926976bcb4cc2285
Daichi0914 <goludrozya44@gmail.com>
2020-11-12 12:05:11 +0900

    1週間の天気予報を表示

--- junk.jsx
+++ junk.jsx
@@ -24,3 +24,43 @@ export const progress = () => {
     </div>
   );
 };
+
+switch (todayW.weather[0].icon) {
+  case '01d':
+    return w01d;
+  case '02d':
+    return w02d;
+  case '03d':
+    return w03d;
+  case '04d':
+    return w04d;
+  case '09d':
+    return w09d;
+  case '10d':
+    return w10d;
+  case '11d':
+    return w11d;
+  case '13d':
+    return w13d;
+  case '50d':
+    return w50d;
+  default:
+    return;
+}
+
+switch (day.getDate()) {
+  case today === day.getDate():
+    return `${todayW.weather[0].icon}`;
+  case today + 1 === day.getDate():
+    return `${yesterdayW.weather[0].icon}`;
+  case today + 2 === day.getDate():
+    return `${tomorrowW.weather[0].icon}`;
+  case today + 3 === day.getDate():
+    return `${threeDaysLaterW.weather[0].icon}`;
+  case today + 4 === day.getDate():
+    return `${fourDaysLaterW.weather[0].icon}`;
+  case today + 5 === day.getDate():
+    return `${fiveDaysLaterW.weather[0].icon}`;
+  case today + 6 === day.getDate():
+    return `${sixDaysLaterW.weather[0].icon}`;
+}
--- src/Components/Atoms/MonthCalendarGrid.jsx
+++ src/Components/Atoms/MonthCalendarGrid.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import { useRecoilValue } from 'recoil';
 
 import { NowYear, NowMonth, Today } from '../../Recoil/DateData';
-import { OneWeekWeatherData, DailyWeatherData } from '../../Recoil/OneWeekWeatherData';
+import { DailyWeatherData } from '../../Recoil/OneWeekWeatherData';
 
 import { makeStyles, Typography } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
+import w01d from '../../Assets/01d.png';
+import w02d from '../../Assets/02d.png';
+import w03d from '../../Assets/03d.png';
+import w04d from '../../Assets/04d.png';
+import w09d from '../../Assets/09d.png';
+import w10d from '../../Assets/10d.png';
+import w11d from '../../Assets/11d.png';
+import w13d from '../../Assets/13d.png';
+import w50d from '../../Assets/50d.png';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    margin: '5px',
+    justifyContent: 'space-between',
+  },
   dayNum: {
     width: 35,
     height: 35,
@@ -25,20 +40,19 @@ const useStyles = makeStyles(theme => ({
     borderRadius: '100px',
     backgroundColor: 'orange',
   },
-  weatherImg: {
+  weatherImgContainer: {
     width: 35,
     height: 35,
     margin: 5,
-    backgroundColor: 'red',
+    // backgroundImage: `url()`,
   },
-}));
+});
 
 const MonthCalendarGrid = ({ day, propsStyle }) => {
   const classes = useStyles();
   const nowYear = useRecoilValue(NowYear);
   const nowMonth = useRecoilValue(NowMonth);
   const today = useRecoilValue(Today);
-  const oneWeekWeather = useRecoilValue(OneWeekWeatherData);
   const {
     todayW,
     yesterdayW,
@@ -48,8 +62,6 @@ const MonthCalendarGrid = ({ day, propsStyle }) => {
     fiveDaysLaterW,
     sixDaysLaterW,
   } = useRecoilValue(DailyWeatherData);
-
-  // console.log(todayW);
 
   const color = () => {
     switch (day.getDay()) {
@@ -62,43 +74,176 @@ const MonthCalendarGrid = ({ day, propsStyle }) => {
     }
   };
 
-  const dateValidation =
-    nowYear === day.getFullYear() &&
-    nowMonth === day.getMonth() &&
-    today === day.getDate();
-
-  const test = nowYear === day.getFullYear() && nowMonth === day.getMonth();
+  const dateValidation = nowYear === day.getFullYear() && nowMonth === day.getMonth();
+  const dateValidationFull = dateValidation && today === day.getDate();
 
   const WeatherImg = () => {
-    if (
-      (test && today === day.getDate()) ||
-      (test && today + 1 === day.getDate()) ||
-      (test && today + 2 === day.getDate()) ||
-      (test && today + 3 === day.getDate()) ||
-      (test && today + 4 === day.getDate()) ||
-      (test && today + 5 === day.getDate()) ||
-      (test && today + 6 === day.getDate())
-    ) {
-      return <div className={classes.weatherImg} />;
+    if (todayW) {
+      if (dateValidation && today === day.getDate()) {
+        if (todayW.weather[0].icon === '01d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w01d}`} />;
+        } else if (todayW.weather[0].icon === '02d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w02d}`} />;
+        } else if (todayW.weather[0].icon === '03d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w03d}`} />;
+        } else if (todayW.weather[0].icon === '04d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w04d}`} />;
+        } else if (todayW.weather[0].icon === '09d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w09d}`} />;
+        } else if (todayW.weather[0].icon === '10d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w10d}`} />;
+        } else if (todayW.weather[0].icon === '11d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w11d}`} />;
+        } else if (todayW.weather[0].icon === '13d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w13d}`} />;
+        } else if (todayW.weather[0].icon === '50d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w50d}`} />;
+        } else {
+          return;
+        }
+      } else if (dateValidation && today + 1 === day.getDate()) {
+        if (yesterdayW.weather[0].icon === '01d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w01d}`} />;
+        } else if (yesterdayW.weather[0].icon === '02d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w02d}`} />;
+        } else if (yesterdayW.weather[0].icon === '03d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w03d}`} />;
+        } else if (yesterdayW.weather[0].icon === '04d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w04d}`} />;
+        } else if (yesterdayW.weather[0].icon === '09d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w09d}`} />;
+        } else if (yesterdayW.weather[0].icon === '10d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w10d}`} />;
+        } else if (yesterdayW.weather[0].icon === '11d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w11d}`} />;
+        } else if (yesterdayW.weather[0].icon === '13d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w13d}`} />;
+        } else if (yesterdayW.weather[0].icon === '50d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w50d}`} />;
+        } else {
+          return;
+        }
+      } else if (dateValidation && today + 2 === day.getDate()) {
+        if (tomorrowW.weather[0].icon === '01d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w01d}`} />;
+        } else if (tomorrowW.weather[0].icon === '02d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w02d}`} />;
+        } else if (tomorrowW.weather[0].icon === '03d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w03d}`} />;
+        } else if (tomorrowW.weather[0].icon === '04d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w04d}`} />;
+        } else if (tomorrowW.weather[0].icon === '09d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w09d}`} />;
+        } else if (tomorrowW.weather[0].icon === '10d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w10d}`} />;
+        } else if (tomorrowW.weather[0].icon === '11d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w11d}`} />;
+        } else if (tomorrowW.weather[0].icon === '13d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w13d}`} />;
+        } else if (tomorrowW.weather[0].icon === '50d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w50d}`} />;
+        } else {
+          return;
+        }
+      } else if (dateValidation && today + 3 === day.getDate()) {
+        if (threeDaysLaterW.weather[0].icon === '01d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w01d}`} />;
+        } else if (threeDaysLaterW.weather[0].icon === '02d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w02d}`} />;
+        } else if (threeDaysLaterW.weather[0].icon === '03d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w03d}`} />;
+        } else if (threeDaysLaterW.weather[0].icon === '04d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w04d}`} />;
+        } else if (threeDaysLaterW.weather[0].icon === '09d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w09d}`} />;
+        } else if (threeDaysLaterW.weather[0].icon === '10d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w10d}`} />;
+        } else if (threeDaysLaterW.weather[0].icon === '11d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w11d}`} />;
+        } else if (threeDaysLaterW.weather[0].icon === '13d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w13d}`} />;
+        } else if (threeDaysLaterW.weather[0].icon === '50d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w50d}`} />;
+        } else {
+          return;
+        }
+      } else if (dateValidation && today + 4 === day.getDate()) {
+        if (fourDaysLaterW.weather[0].icon === '01d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w01d}`} />;
+        } else if (fourDaysLaterW.weather[0].icon === '02d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w02d}`} />;
+        } else if (fourDaysLaterW.weather[0].icon === '03d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w03d}`} />;
+        } else if (fourDaysLaterW.weather[0].icon === '04d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w04d}`} />;
+        } else if (fourDaysLaterW.weather[0].icon === '09d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w09d}`} />;
+        } else if (fourDaysLaterW.weather[0].icon === '10d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w10d}`} />;
+        } else if (fourDaysLaterW.weather[0].icon === '11d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w11d}`} />;
+        } else if (fourDaysLaterW.weather[0].icon === '13d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w13d}`} />;
+        } else if (fourDaysLaterW.weather[0].icon === '50d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w50d}`} />;
+        } else {
+          return;
+        }
+      } else if (dateValidation && today + 5 === day.getDate()) {
+        if (fiveDaysLaterW.weather[0].icon === '01d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w01d}`} />;
+        } else if (fiveDaysLaterW.weather[0].icon === '02d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w02d}`} />;
+        } else if (fiveDaysLaterW.weather[0].icon === '03d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w03d}`} />;
+        } else if (fiveDaysLaterW.weather[0].icon === '04d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w04d}`} />;
+        } else if (fiveDaysLaterW.weather[0].icon === '09d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w09d}`} />;
+        } else if (fiveDaysLaterW.weather[0].icon === '10d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w10d}`} />;
+        } else if (fiveDaysLaterW.weather[0].icon === '11d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w11d}`} />;
+        } else if (fiveDaysLaterW.weather[0].icon === '13d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w13d}`} />;
+        } else if (todayW.weather[0].icon === '50d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w50d}`} />;
+        } else {
+          return;
+        }
+      } else if (dateValidation && today + 6 === day.getDate()) {
+        if (sixDaysLaterW.weather[0].icon === '01d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w01d}`} />;
+        } else if (sixDaysLaterW.weather[0].icon === '02d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w02d}`} />;
+        } else if (sixDaysLaterW.weather[0].icon === '03d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w03d}`} />;
+        } else if (sixDaysLaterW.weather[0].icon === '04d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w04d}`} />;
+        } else if (sixDaysLaterW.weather[0].icon === '09d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w09d}`} />;
+        } else if (sixDaysLaterW.weather[0].icon === '10d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w10d}`} />;
+        } else if (sixDaysLaterW.weather[0].icon === '11d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w11d}`} />;
+        } else if (sixDaysLaterW.weather[0].icon === '13d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w13d}`} />;
+        } else if (sixDaysLaterW.weather[0].icon === '50d') {
+          return <img alt='天気' className={classes.weatherImgContainer} src={`${w50d}`} />;
+        } else {
+          return;
+        }
+      } else {
+        return;
+      }
     } else {
       return;
     }
   };
 
-  // const OneWeekWeather = () => {
-  //   switch (key) {
-  //     case value:
-
-  //       break;
-
-  //     default:
-  //       break;
-  //   }
-  // }
-
   return (
-    <div style={{ display: 'flex', margin: '5px', justifyContent: 'space-between' }}>
-      <div className={dateValidation ? classes.todayNum : classes.dayNum}>
+    <div className={classes.root}>
+      <div className={dateValidationFull ? classes.todayNum : classes.dayNum}>
         <Typography style={{ color: propsStyle ? propsStyle : color() }}>
           {propsStyle ? `${day.getMonth() + 1}/${day.getDate()}` : `${day.getDate()}日`}
         </Typography>
