7a668fb8285a58961a5727eeb41b23d35a6a9c5c
Daichi0914 <goludrozya44@gmail.com>
2020-11-15 04:59:18 +0900

    ヘッダーメニュー全ページ対応

--- src/Components/WeekCalendar/WeekCalendar.jsx
+++ src/Components/WeekCalendar/WeekCalendar.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core';
+import { DrawerWidth } from '../../Recoil/DrawerWidth';
+import { MenuDrawerState } from '../../Recoil/MenuDrawerState';
+
+const useStyles = makeStyles(theme => ({
+  gridList: {
+    height: `calc(100vh - 210px)`,
+    padding: '40px 30px',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth => drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+}));
 
 const WeekCalendar = () => {
-  return <>週表示</>;
+  const drawerWidth = useRecoilValue(DrawerWidth);
+  const classes = useStyles(drawerWidth);
+  const drawerOpen = useRecoilValue(MenuDrawerState);
+
+  return (
+    <div
+      className={clsx(classes.appBar, {
+        [classes.appBarShift]: drawerOpen,
+      })}
+    >
+      週表示
+    </div>
+  );
 };
 
 export default WeekCalendar;
