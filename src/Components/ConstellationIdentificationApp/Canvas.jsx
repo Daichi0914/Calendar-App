262b80d521a40ac9f344953a6fa7fa449747070e
Daichi0914 <DaichiSugawara44@gmail.com>
2021-02-07 06:46:39 +0900

    :triangular_flag_on_post: Add a feature to pick up pixels with high brightness

--- src/Components/ConstellationIdentificationApp/Canvas.jsx
+++ src/Components/ConstellationIdentificationApp/Canvas.jsx
@@ -8,9 +8,8 @@ const Canvas = () => {
   const [isBlur, setIsBlur] = useState(false);
   const [isGrayScale, setIsGrayScale] = useState(false);
   const [isGetPixelData, setIsGetPixelData] = useState(null);
-  const [pixelsData, setPixelsData] = useState(null);
-  const [imgData, setImgData] = useState(null);
-  const [brightnessSortData, setBrightnessSortData] = useState(null);
+  // const [pixelsData, setPixelsData] = useState(null);
+  const [topBrightnessPixelData, setTopBrightnessPixelData] = useState(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -34,56 +33,72 @@ const Canvas = () => {
         drawImage();
       }
 
-      setImgData(context.getImageData(0, 0, canvas.width, canvas.height));
+      const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+      const pixelCoordinate = [];
 
       if (isGrayScale) {
-        turnOnGrayScale(canvas.width);
+        const pixels = imgData.data;
+        const gray_scale_threshold = 100;
+
+        for (let i = 0; i < pixels.length; i += 4) {
+          let lightness = parseInt(
+            // 輝度y = 0.299r + 0.587g + 0.114b
+            pixels[i] * 0.299 + pixels[i + 1] * 0.587 + pixels[i + 2] * 0.114
+          );
+          if (lightness > gray_scale_threshold) {
+            pixels[i] = lightness;
+            pixels[i + 1] = lightness;
+            pixels[i + 2] = lightness;
+            // X座標,Y座標
+            let x = (i / 4) % canvas.width;
+            let y = Math.floor(i / 4 / canvas.width);
+            pixelCoordinate.push([x, y, lightness]);
+          } else {
+            pixels[i] = 0;
+            pixels[i + 1] = 0;
+            pixels[i + 2] = 0;
+          }
+        }
         context.putImageData(imgData, 0, 0);
         drawImage();
       }
-    };
 
-    if (isGetPixelData) {
-      sortLightness();
-    }
+      if (isGetPixelData) {
+        const brightnessSort = pixelCoordinate.sort((a, b) => {
+          // [2]は輝度値
+          if (a[2] > b[2]) return -1;
+          if (a[2] < b[2]) return 1;
+          return 0;
+        });
+
+        const get_number_of_high_brightness = 5;
+        const pixel_range_threshold = 100;
+        let copyBrightnessSort = brightnessSort; // brightnessSortを直接いじるのはまずいのでコピー
+        let highBrightness = [];
+
+        for (let i = 0; i < get_number_of_high_brightness; i++) {
+          let highestBrightnessPixel = copyBrightnessSort[0];
+          highBrightness.push(highestBrightnessPixel);
+          copyBrightnessSort = copyBrightnessSort.filter(
+            pixelData =>
+              // X,Yそれぞれ+-pixelRangeThreshold分は同じ星と考えられるため除外 除外 -> !()
+              !(
+                pixelData[0] < highestBrightnessPixel[0] + pixel_range_threshold &&
+                pixelData[0] > highestBrightnessPixel[0] - pixel_range_threshold &&
+                pixelData[1] < highestBrightnessPixel[1] + pixel_range_threshold &&
+                pixelData[1] > highestBrightnessPixel[1] - pixel_range_threshold
+              )
+          );
+        }
+        setTopBrightnessPixelData(highBrightness);
+      }
+    };
 
     img.onload = drawImageActualSize;
     img.style.display = 'none';
   }, [fileUrl, isBlur, isGrayScale, isGetPixelData]);
 
-  const turnOnGrayScale = canvasWidth => {
-    const pixels = imgData.data;
-    const threshold = 100;
-    const pixelCoordinate = [];
-    for (let i = 0; i < pixels.length; i += 4) {
-      let lightness = parseInt(
-        // 輝度y = 0.299r + 0.587g + 0.114b
-        pixels[i] * 0.299 + pixels[i + 1] * 0.587 + pixels[i + 2] * 0.114
-      );
-      if (lightness > 200) {
-        pixels[i] = lightness > threshold ? lightness : 0;
-        pixels[i + 1] = lightness > threshold ? lightness : 0;
-        pixels[i + 2] = lightness > threshold ? lightness : 0;
-        // X座標,Y座標
-        let x = (i / 4) % canvasWidth;
-        let y = Math.floor(i / 4 / canvasWidth);
-        pixelCoordinate.push([x, y, lightness]);
-      }
-    }
-    setPixelsData(pixelCoordinate);
-  };
-
-  const sortLightness = () => {
-    const brightnessSort = pixelsData.sort((a, b) => {
-      // [2]は輝度値を入れてある
-      if (a[2] > b[2]) return -1;
-      if (a[2] < b[2]) return 1;
-      return 0;
-    });
-    setBrightnessSortData(brightnessSort);
-
-    // const 
-  };
+  console.log(topBrightnessPixelData);
 
   // contextがスコープ外なのでここでCanvas処理は書けない
   const handleBlurClick = () => {
@@ -102,8 +117,6 @@ const Canvas = () => {
     setIsGetPixelData(!isGetPixelData);
   };
   ///////////////////////////////////////////////////
-
-  console.log(brightnessSortData);
 
   return (
     <>
